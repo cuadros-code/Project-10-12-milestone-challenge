b382498e933f2402fa26e2649562a900341c68f7
kevin cuadros <kevindavidcuadros@gmail.com>
2024-09-08 17:24:44 -0500

    add save function

--- Project-10-12-milestone-challenge/ViewController.swift
+++ Project-10-12-milestone-challenge/ViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+enum EditType {
+    case title
+    case subtitle
+}
+
 class ViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var listImages = [FileModel]()
@@ -22,10 +27,20 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate, UI
             action: #selector(takePhoto)
         )
         
+        let defaults = UserDefaults.standard
+        
+        if let getList = defaults.object(forKey: "listImages") as? Data {
+            let jsonDecoder = JSONDecoder()
+            do {
+                listImages = try jsonDecoder.decode([FileModel].self, from: getList)
+            } catch {
+                print("Failed to get List.")
+            }
+        }
+        
     }
     
     // Table Actions
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return listImages.count
     }
@@ -45,11 +60,12 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate, UI
     }
     
     override func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
-        let editTitleAction = UIAction(title: "Edit title", image: UIImage(systemName: "pencil")) { _ in
-            
+        
+        let editTitleAction = UIAction(title: "Edit title", image: UIImage(systemName: "pencil")) { [weak self] _ in
+            self?.changeTitle(at: indexPath, type: .title)
         }
-        let editSecondaryText = UIAction(title: "Edit subtitle", image: UIImage(systemName: "pencil")) { _ in
-            
+        let editSecondaryText = UIAction(title: "Edit subtitle", image: UIImage(systemName: "pencil")) { [weak self] _ in
+            self?.changeTitle(at: indexPath, type: .subtitle)
         }
         
         let deleteItem = UIAction(title: "Delete", image: UIImage(systemName: "trash")) { [weak self] _ in
@@ -96,7 +112,7 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate, UI
         DispatchQueue.main.async {
             self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
         }
-        
+        saveList(listImages)
         dismiss(animated: true)
     }
     
@@ -108,19 +124,57 @@ class ViewController: UITableViewController, UIImagePickerControllerDelegate, UI
     
     // Alerts
     func confirmDeleteItem(at indexPath: IndexPath){
-        let alert = UIAlertController(title: "Delete Item", message: "Be sure to delete", preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: "This photo will be delete on your device", message: nil, preferredStyle: .actionSheet)
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        let deleteAction = UIAlertAction(title: "Delete", style: .default) { [weak self] _ in
-            self?.listImages.remove(at: indexPath.row)
-            self?.tableView.deleteRows(at: [indexPath], with: .fade)
+        let deleteAction = UIAlertAction(title: "Delete", style: .default) { _ in
+            self.listImages.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            self.saveList(self.listImages)
         }
-        
         deleteAction.setValue(UIColor.red, forKey: "titleTextColor")
         
         alert.addAction(deleteAction)
         alert.addAction(cancelAction)
         present(alert, animated: true)
+    }
+    
+    func changeTitle(at indexPath: IndexPath, type typeAlert: EditType){
+        
+        let message = typeAlert == .title ? "Rename your photo" : "Add a caption to your photo"
+        
+        let alert = UIAlertController(title: "\(message)", message: nil, preferredStyle: .alert)
+        alert.addTextField()
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        let submitAction = UIAlertAction(title: "Save", style: .default) { [weak alert] _ in
+            if let text = alert?.textFields?[0].text {
+                if typeAlert == .title {
+                    self.listImages[indexPath.row].title = text
+                } else if typeAlert == .subtitle {
+                    self.listImages[indexPath.row].subtitle = text
+                }
+                self.tableView.reloadRows(at: [indexPath], with: .fade)
+            }
+            self.saveList(self.listImages)
+        }
+        
+        alert.addAction(submitAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
+    
+    
+    func saveList( _ list: [FileModel]) {
+        let jsonEncoder = JSONEncoder()
+        if let saveData = try? jsonEncoder.encode(list) {
+            let defaults = UserDefaults.standard
+            defaults.setValue(saveData, forKey: "listImages")
+        } else {
+            print("Failed to save photo.")
+        }
+        
     }
 
 }
